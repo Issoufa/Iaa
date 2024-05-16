4a774c070d7e5f872a3f90049b21a419fb543e8b
iaaha <issoufou1abdou1@gmail.com>
2024-05-15 21:42:17 -0400

    filiation file

---
+++ filiation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Individu:
+    def __init__(self, birth_day, name, first_name):
+        self.birth_day = birth_day
+        self.name = name
+        self.first_name = first_name
+        self.son = None
+        self.daughter = None
+        self.father = None
+        self.mother = None
+
+    def add_son(self, son_name, son_first_name, son_birth_day):
+        pass
+
+    def add_daughter(self, daughter_name, daughter_first_name, daughter_birth_day):
+        pass
+
+    def ddd(self):
+        pass
